c04a867a17ca991a8fa7a0662ca09b7e3b046fd6
Daniel Schulman <danieljschulman@gmail.com>
2011-10-03 00:56:05 -0400

    handling morphology with simplenlg

--- src/main/java/fancytalk/Embiggen.java
+++ src/main/java/fancytalk/Embiggen.java
@@ -8,12 +8,21 @@ import net.sf.extjwnl.data.POS;
 import net.sf.extjwnl.data.Synset;
 import net.sf.extjwnl.data.Word;
 import net.sf.extjwnl.dictionary.Dictionary;
+import simplenlg.features.Feature;
+import simplenlg.features.NumberAgreement;
+import simplenlg.features.Person;
+import simplenlg.features.Tense;
+import simplenlg.framework.InflectedWordElement;
+import simplenlg.framework.LexicalCategory;
+import simplenlg.morphology.english.MorphologyProcessor;
 
 public class Embiggen {
     private final Dictionary dictionary;
+    private final MorphologyProcessor morphologizer;
 
     public Embiggen(Dictionary dictionary) {
 	this.dictionary = dictionary;
+	this.morphologizer = new MorphologyProcessor();
     }
 
     public List<List<String>> applyAllAll(List<List<Token>> in) 
@@ -45,8 +54,9 @@ public class Embiggen {
 	for (Synset syn: iw.getSenses())
 	    for (Word word: syn.getWords()) {
 		String lemma = word.getLemma();
-		if (lemma.length() > biggest.length())
-		    biggest = lemma;
+		String morphed = doMorphology(lemma, in.tag);
+		if (morphed.length() > biggest.length())
+		    biggest = morphed;
 	    }
 
 	return biggest;
@@ -61,6 +71,40 @@ public class Embiggen {
 	    return POS.NOUN;
 	else if (tag.startsWith("VB"))
 	    return POS.VERB;
+	else
+	    return null;
+    }
+
+    private String doMorphology(String base, String tag) {
+	LexicalCategory cat = asLexicalCategory(tag);
+	if (cat==null)
+	    return base;
+
+	InflectedWordElement iwe = new InflectedWordElement(base, cat);
+
+	if (tag.equals("NNS"))
+	    iwe.setFeature(Feature.NUMBER, NumberAgreement.PLURAL);
+	if (tag.equals("JJR") || tag.equals("RBR"))
+	    iwe.setFeature(Feature.IS_COMPARATIVE, true);
+	if (tag.equals("JJS") || tag.equals("RBS"))
+	    iwe.setFeature(Feature.IS_SUPERLATIVE, true);
+	if (tag.equals("VBD"))
+	    iwe.setFeature(Feature.TENSE, Tense.PAST);
+	if (tag.equals("VBP"))
+	    iwe.setFeature(Feature.PERSON, Person.FIRST);
+
+	return morphologizer.realise(iwe).getRealisation();
+    }
+
+    private LexicalCategory asLexicalCategory(String tag) {
+	if (tag.startsWith("JJ"))
+	    return LexicalCategory.ADJECTIVE;
+	else if (tag.startsWith("RB"))
+	    return LexicalCategory.ADVERB;
+	else if (tag.startsWith("NN"))
+	    return LexicalCategory.NOUN;
+	else if (tag.startsWith("VB"))
+	    return LexicalCategory.VERB;
 	else
 	    return null;
     }
